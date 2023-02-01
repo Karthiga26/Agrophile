e282440b5bdd10abdbc23a217aa0aed9e6eec019
Illakia <118658309+Illakia@users.noreply.github.com>
2023-02-01 17:52:52 +0530

    Add files via upload

---
+++ agrophile/views.py
@@ -0,0 +1,109 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+import requests
+import numpy as np
+import config
+import pickle
+model_path = 'savedmodels/RandomForest.pkl'
+model = pickle.load(
+    open(model_path, 'rb'))
+model = pickle.load(open('savedmodels/classifier.pkl','rb'))
+ferti = pickle.load(open('savedmodels/fertilizer.pkl','rb'))
+def home(request):
+    return render(request,'home.html')
+"""def login(request):
+
+    if request.method == 'POST':
+        username=request.POST['username']
+        email=request.POST['email']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'username Taken')
+                return redirect('login')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'email Taken')
+                return redirect('login')
+            else:
+                user=User.objects.create_user(username=username,email=email,password=password1)
+                user.save()
+                print('user created')
+        else:
+            messages.info(request,'password not matching....')    
+            return redirect('login')
+        return redirect('/')
+    else:    
+        return render(request,'login.html')"""
+    
+def about(request):
+    return render(request,'about.html')
+def prediction(request):
+    return render(request,'prediction.html')
+def contact(request):
+    return render(request,'contact.html')
+def info(request):
+    return render(request,'info.html')    
+def fertilizer(request):
+    return render(request,'index.html') 
+def weather_fetch(city_name):
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+def crop_prediction(request):
+    #title = 'Harvestify - Crop Recommendation'
+
+    if request.method == 'POST':
+        N = int(request.POST['nitrogen'])
+        P = int(request.POST['phosphorous'])
+        K =int( request.POST['pottasium'])
+        ph =float(request.POST['ph'])
+        rainfall =float(request.POST['rainfall'])
+
+        
+        #state = request.POST["State"]
+        
+        city = str(request.POST['City'])
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            my_prediction = model.predict(data)
+            final_prediction = my_prediction[0]
+
+            return render(request,'result.html', {'prediction':final_prediction})
+
+        else:
+
+            return render(request,'error.html')    
+def fertilizer_predict(request):
+   
+   if request.method=='POST':
+     temp = int(request.POST['temp'])
+     humi = int(request.POST['humid'])
+     mois = int(request.POST['mois'])
+     soil = str(request.POST['soil'])
+     crop = str(request.POST['crop'])
+     nitro = int(request.POST['nitro'])
+     pota = int(request.POST['pota'])
+     phosp = int(request.POST['phos'])
+     input = np.array([[temp,humi,mois,soil,crop,nitro,pota,phosp]])
+
+     res = ferti.classes_[model.predict(input)]
+
+     return render(request,'fertilizer-result.html',{'recommendation':res})    
+    
