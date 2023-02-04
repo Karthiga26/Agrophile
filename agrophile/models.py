bbf7ed0e2b357615de60aa06a50a6e3a1db7e51e
Karthiga26 <110459113+Karthiga26@users.noreply.github.com>
2023-02-04 11:49:19 +0530

    Delete models.py

--- agrophile/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Crops(models.Model):
-    cropname = models.CharField(max_length=100)
-    cropimg =  models.ImageField(upload_to='exercise_pics')
-    cropdetails = models.TextField()
-    
